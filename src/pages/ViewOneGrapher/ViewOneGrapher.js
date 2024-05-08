b44d462b2fc2378488b1d8f68549d0720b906881
omarali18 <omarali1814000@gmail.com>
2024-05-08 23:32:26 +0600

    add calendar

--- src/pages/ViewOneGrapher/ViewOneGrapher.js
+++ src/pages/ViewOneGrapher/ViewOneGrapher.js
@@ -2,15 +2,34 @@ import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from '../Shared/Navbar/Navbar';
 import Footer from '../Shared/Footer/Footer';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 // import dayjs from 'dayjs';
 // import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 // import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 // import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 
+// 
+// type ValuePiece = Date | null;
+
+// type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+
 const ViewOneGrapher = () => {
     const [singleGrapher, setSingleGrapher] = useState({})
+    const [date, setDate] = useState(new Date())
     const idIs = useParams();
+    // const [value, onChange] = useState<Value>(new Date());
     // const history = useHistory();
+
+    // type ValuePiece = Date | null;
+
+// type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+    const onChange = date =>{
+        setDate(date);
+    }
 
     useEffect(() => {
         fetch(`http://localhost:5000/allGrapher?id=${idIs.id}`)
@@ -28,6 +47,11 @@ const ViewOneGrapher = () => {
                 <div className='border border-success p-2 m-3'>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo deserunt quae voluptates voluptatibus sequi nobis enim inventore dolores, illo voluptatem ratione possimus fugiat atque debitis saepe deleniti distinctio quas beatae.</p>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo deserunt quae voluptates voluptatibus sequi nobis enim inventore dolores, illo voluptatem ratione possimus fugiat atque debitis saepe deleniti distinctio quas beatae.</p>
+
+                    <Calendar onChange={onChange} value={date} />
+                    {console.log(date)}
+
+
                     {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <StaticDatePicker
                         displayStaticWrapperAs="desktop"
